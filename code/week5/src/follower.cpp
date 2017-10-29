7b04641a8c73bc25b593202644ce4042581938ed
Jason Gibson <jgibson37@gatech.edu>
2017-10-29 14:12:47 -0400

    Revert "removed solution"
    This reverts commit 79c54c1ecc5f885dcb4567f32ed4f7d4be62486e.

--- code/week5/src/follower.cpp
+++ code/week5/src/follower.cpp
@@ -3,7 +3,39 @@
 #include <std_msgs/String.h>
 #include <tf/transform_listener.h>
 
-int main(int argc, char** argv) {
+ros::Publisher motor_pub;
+std::string topic_name;
 
+void linkNameCallback(const std_msgs::String::ConstPtr& msg)
+{
+  topic_name = msg->data;
+}
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "follower_2");
+
+  ros::NodeHandle nh;
+
+  motor_pub = nh.advertise<hal_msgs::speed_pair>("/motors", 1);
+
+  ros::Subscriber link_sub = nh.subscribe("/link", 1, linkNameCallback);
+
+  ros::Rate loop_rate(10);
+
+  while(ros::ok()) {
+    tf::TransformListener tf_listener;
+    tf::StampedTransform transform;
+    if (tf_listener.waitForTransform("/base_link", "/ball", ros::Time(0), ros::Duration(3.0)))
+    {
+      tf_listener.lookupTransform("/base_link", "/ball", ros::Time(0), transform);
+      double roll, pitch, yaw;
+      tf::Matrix3x3(transform.getRotation()).getRPY(roll, pitch, yaw);
+      ROS_INFO_STREAM(transform.getOrigin().getX());
+      ROS_INFO_STREAM(roll << " " << pitch << " " << yaw);
+    }
+
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
   return 0;
 }
