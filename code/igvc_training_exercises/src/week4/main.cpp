e04b111281daae32a22c03be8a2427f090e1f4f5
Oswin So <oswinso@gmail.com>
2019-10-27 18:24:10 -0400

    Added answers for week 4 and week 5

--- code/igvc_training_exercises/src/week4/main.cpp
+++ code/igvc_training_exercises/src/week4/main.cpp
@@ -1,3 +1,106 @@
+#include <ros/ros.h>
+
+#include <nav_msgs/Odometry.h>
+#include <sensor_msgs/Imu.h>
+#include <tf/transform_datatypes.h>
+#include <tf/transform_broadcaster.h>
+
+// struct to represent Position
+struct Position
+{
+  double x;
+  double y;
+};
+
+// struct to represent a Pose: a Position + heading
+struct Pose
+{
+  Position position;
+  double heading;
+};
+
+// struct to represent a twist, ie. a linear / angular velocity
+struct Twist
+{
+  double linear;
+  double angular;
+};
+
+// struct to represent the State
+struct State
+{
+  Pose pose;
+  Twist twist;
+};
+
+State g_state; // Current state
+
+ros::Time g_last_time; // Last time we received a message, to calculate dt
+ros::Publisher g_odom_pub; // Publisher for odom
+
+/**
+ * Callback for imu message
+ * @param msg message containing imu
+ */
+void imuCallback(const sensor_msgs::Imu& msg)
+{
+  // tf Transform Broadcaster to broadcast tf transforms. We make it static here to make sure that
+  // it happens after the first node handle
+  static tf::TransformBroadcaster g_broadcaster;
+
+  // Calculate the delta_t from current message to the last message
+  ros::Duration delta_t = msg.header.stamp - g_last_time;
+
+  // Set the twist to be equal to the result from the gyroscope
+  g_state.twist.angular = msg.angular_velocity.z;
+
+  // The linear velocity is the integral of acceleration
+  g_state.twist.linear += msg.linear_acceleration.x * delta_t.toSec();
+  // The heading is the integral of angular velocity
+  g_state.pose.heading += g_state.twist.angular * delta_t.toSec();
+
+  // The x and y of the position is just the integral of velocity multiplied by cos / sin
+  g_state.pose.position.x += cos(g_state.pose.heading) * g_state.twist.linear * delta_t.toSec();
+  g_state.pose.position.y += sin(g_state.pose.heading) * g_state.twist.linear * delta_t.toSec();
+
+  g_last_time = msg.header.stamp; // update the last timestamp
+
+  // Fill in the information for the nav_msgs::Odometry msg
+  nav_msgs::Odometry odometry_msg;
+  odometry_msg.header.frame_id = "odom";
+  odometry_msg.header.stamp = msg.header.stamp;
+  odometry_msg.pose.pose.position.x = g_state.pose.position.x;
+  odometry_msg.pose.pose.position.y = g_state.pose.position.y;
+  // We use tf::createQuaternionMsgFromYaw to convert from a heading to a quaternion message
+  odometry_msg.pose.pose.orientation = tf::createQuaternionMsgFromYaw(g_state.pose.heading);
+  odometry_msg.twist.twist.linear.x = g_state.twist.linear;
+  odometry_msg.twist.twist.angular.z = g_state.twist.angular;
+  // And publish it
+  g_odom_pub.publish(odometry_msg);
+
+  // Fill in the information for the tf::Transform variable
+  tf::Transform transform;
+  transform.setOrigin({g_state.pose.position.x, g_state.pose.position.y, 0.0});
+  // Use tf::createQuaternionFromYaw to convert from a heading to a tf quaternion
+  transform.setRotation(tf::createQuaternionFromYaw(g_state.pose.heading));
+  tf::StampedTransform stamped_transform{transform, msg.header.stamp, "odom", "oswin"};
+  // So that we can broadcast it for other nodes to use
+  g_broadcaster.sendTransform(stamped_transform);
+}
+
 int main(int argc, char** argv)
 {
+  ros::init(argc, argv, "week_4");
+
+  ros::NodeHandle nh;
+
+  // Subscribe to oswin/imu and publish to oswin/odometry
+  ros::Subscriber imu_sub = nh.subscribe("oswin/imu", 1, imuCallback);
+  g_odom_pub = nh.advertise<nav_msgs::Odometry>("oswin/odometry", 1);
+
+  // Set the last_time to now
+  g_last_time = ros::Time::now();
+
+  // Don't forget to spin to let ros handle calling the callbacks
+  ros::spin();
 }
--- code/igvc_training_exercises/src/week5/main.cpp
+++ code/igvc_training_exercises/src/week5/main.cpp
@@ -1,3 +1,90 @@
+#include <ros/ros.h>
+
+#include <nav_msgs/OccupancyGrid.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl_ros/point_cloud.h>
+#include <pcl_ros/transforms.h>
+#include <tf/transform_listener.h>
+
+ros::Publisher g_map_pub;
+nav_msgs::OccupancyGrid g_map;
+
+void updateMap(const pcl::PointCloud<pcl::PointXYZ>& pointcloud)
+{
+  // Create a static tf::TransformListener so that we only create it once, and it's after ros::NodeHandle
+  static tf::TransformListener transform_listener;
+
+  // Convert the timestamp from PCL to ros::Time, since PCL uses uint64
+  ros::Time stamp = pcl_conversions::fromPCL(pointcloud.header.stamp);
+
+  // Lookup the transform to convert the pointcloud from oswin to odom, at the time of the message
+  // if it is valid, otherwise get the latest transform
+  if (!transform_listener.waitForTransform("odom", "oswin", stamp, ros::Duration(5)))
+  {
+    ROS_ERROR_STREAM("Failed to find transform from odom to oswin.");
+    return;
+  }
+  tf::StampedTransform transform_to_odom;
+  transform_listener.lookupTransform("odom", "oswin", stamp, transform_to_odom);
+
+  // Transform the pointcloud from the lidar frame to the odom frame using the transform form just now
+  pcl::PointCloud<pcl::PointXYZ> transformed_pointcloud;
+  pcl_ros::transformPointCloud(pointcloud, transformed_pointcloud, transform_to_odom);
+
+  // Find the coordinate in the grid from each point in the pointcloud
+  for (const auto& point : transformed_pointcloud)
+  {
+    int map_x = static_cast<int>(std::round((point.x - g_map.info.origin.position.x) / g_map.info.resolution));
+    int map_y = static_cast<int>(std::round((point.y - g_map.info.origin.position.y) / g_map.info.resolution));
+
+    // Convert the indices from 2D index to 1D index
+    int index = map_y * g_map.info.width + map_x;
+
+    // If it's less than 100, increment it
+    if (g_map.data[index] < 100)
+    {
+      g_map.data[index]++;
+    }
+  }
+}
+
+/**
+ * The callback for the pointcloud subscriber
+ * @param pointcloud
+ */
+void pointcloudCallback(const pcl::PointCloud<pcl::PointXYZ>& pointcloud)
+{
+  g_map.header.stamp = pcl_conversions::fromPCL(pointcloud.header.stamp);
+  // We break out the "updateMap" functionality into another method for clarity
+  updateMap(pointcloud);
+  // Publish the updated map
+  g_map_pub.publish(g_map);
+}
+
 int main(int argc, char** argv)
 {
+  ros::init(argc, argv, "week5");
+
+  ros::NodeHandle nh;
+
+  // Advertise to oswin/map and subscriber to oswin/pointcloud
+  g_map_pub = nh.advertise<nav_msgs::OccupancyGrid>("oswin/map", 1);
+  ros::Subscriber pointcloud_sub = nh.subscribe("oswin/pointcloud", 1, &pointcloudCallback);
+
+  // Set information about the map, ie. width, height etc
+  g_map.info.width = 200;
+  g_map.info.height = 200;
+  g_map.info.origin.position.x = -10;
+  g_map.info.origin.position.y = -10;
+  g_map.info.origin.orientation = tf::createQuaternionMsgFromYaw(0.0);
+  g_map.info.resolution = 0.1;
+  g_map.header.frame_id = "odom"; // Make sure to set the frame_id
+
+  int total_cells = g_map.info.width * g_map.info.height;
+
+  // Instantiate the g_map.data to hvae total_cells number of cells
+  g_map.data = std::vector<int8_t>(total_cells, 0);
+
+  // Don't forget to ros::spin() to let ros call callbacks for you
+  ros::spin();
 }
